e0cd4f9d5dc95d64a936b5f8dfedac3ca01d07ab
VVSD Charan <vvsdcharan2001@gmail.com>
2023-06-11 12:40:25 +0530

    Removed unused page and code used for testing

--- app.js
+++ app.js
@@ -18,14 +18,6 @@ Whether you're a seasoned tech professional or just starting your journey into t
 
 Join our vibrant community of tech enthusiasts, innovators, and curious minds as we explore groundbreaking inventions, emerging trends, and transformative ideas. Get ready to immerse yourself in the world of TechBuzz, where knowledge meets inspiration and technology takes center stage.`;
 
-// About page display content 
-const aboutContent = `Welcome to TechBuzz, your ultimate destination for staying up-to-date with the latest trends in technology. Our blog is dedicated to providing insightful and engaging content that covers a wide range of topics related to trending technologies.
-
-At TechBuzz, we believe that technology is constantly evolving, and it's essential to stay informed about the latest developments shaping our digital world. Our team of passionate tech enthusiasts and industry experts curates thought-provoking articles, in-depth analyses, and practical guides to help you navigate the ever-changing landscape of technology.
-
-Whether you're a seasoned tech professional or just starting your journey into the digital realm, TechBuzz offers something for everyone. From artificial intelligence and blockchain to virtual reality and cybersecurity, we delve into the most exciting and disruptive technologies of our time. Our aim is to simplify complex concepts and make them accessible to our readers, empowering them to embrace the possibilities and opportunities that technology presents.
-
-Join our vibrant community of tech enthusiasts, innovators, and curious minds as we explore groundbreaking inventions, emerging trends, and transformative ideas. Get ready to immerse yourself in the world of TechBuzz, where knowledge meets inspiration and technology takes center stage.`;
 
 // Contact page display content 
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
@@ -54,26 +46,6 @@ const postSchema=new mongoose.Schema({
 });
 //Model
 const Post=mongoose.model('blogPost',postSchema);
-//Some data to test
-// const post1=new Post({
-//    title:"Python",
-//    content:"Python is usually used for creating sites and programming, task robotization, information investigation, and information representation. Since it's moderately simple to learn, Python has been taken on by numerous non-software engineers like bookkeepers and researchers"
-// });
-// const post2=new Post({
-//    title:"JavaScript",
-//    content:"JavaScript is typically inserted into HTML when used on the web, either directly in the file in an HTML tag, or linkedto a separate file containing the script. JavaScript, as a full featured scripting language, can be used to provide functionality to a website. "
-// });
-// Post.insertMany([post1,post2]).then((data)=>{
-//    console.log("Insertion successful");
-// }).catch((err)=>{
-//    console.log("Insertion failed");
-// });
-// Post.find().then((data)=>{
-//    console.log(data);
-// }).catch((err)=>{
-//    console.log(err);
-// });
-
 
 app.listen(3000, (err)=>{
    if(err){
@@ -84,15 +56,6 @@ app.listen(3000, (err)=>{
 
 app.get('/posts/:topic',(request,response)=>{
     var topic=_.lowerCase(request.params.topic);
-
-   //  for(let i=0;i<posts.length;i++)
-   //  {
-   //    if(_.lowerCase(topic)===_.lowerCase(posts[i].title))
-   //    {
-   //       var post=posts[i];
-   //       response.render("post",{post});
-   //    }
-   //  }
 
     Post.findOne({titlelc:topic}).then((Blogpost)=>{
        response.render("post",{Blogpost});
@@ -109,10 +72,6 @@ app.get('/',(request,response)=>{
     })
 });
 
-app.get('/about',(request,response)=>{
-  response.render("about",{aboutContent});
-});
-
 app.get('/contact',(request,response)=>{
   response.render("contact",{contactContent});
 });
@@ -122,12 +81,6 @@ app.get('/compose',(request,response)=>{
 });
 
 app.post('/compose',(request,response)=>{
-   // const post = {
-   //    title : request.body.postTitle,
-   //    content : request.body.postBody
-   // };
-   // posts.push(post);
-
    const post = new Post({
       title : request.body.postTitle,
       titlelc : _.lowerCase(request.body.postTitle),
